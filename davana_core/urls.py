6cf75cd704ec9e04099c433269feb3f50117691b
Avin Kavish <avinkavish@proton.me>
2026-06-30 21:04:22 +0530

    add /api prefix to urls

--- davana_core/urls.py
+++ davana_core/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("system-admin/", admin.site.urls),
-    path("v1/", include("davana_core.v1_urls")),
+    path("/api/system-admin/", admin.site.urls),
+    path("/api/v1/", include("davana_core.v1_urls")),
 ]
 
 if settings.DEBUG:
